628a336a49187f480f9227ffcda5adc5a1b4b793
Omenka-da-boss <omenkandubuisi@gmail.com>
2025-07-31 12:17:44 +0100

    update prices

--- bisi/index.js
+++ bisi/index.js
@@ -10,8 +10,8 @@ app.get('/api/products',(req,res)=>{
     const products = [
         {id:1,name:'Laptop',price:6000},
         {id:2,name:'Desktop',price:7500},
-        {id:3,name:'Phone',price:900},
-        {id:4,name:'Netflix',price:3000},
+        {id:3,name:'Phone',price:90000},
+        {id:4,name:'Netflix',price:113000},
     ]
     res.status(200).json({products})
 })
