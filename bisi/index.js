da7e739ff4e91ad7eab0349f10d6aa04afe00a1e
Omenka-da-boss <omenkandubuisi@gmail.com>
2025-07-31 11:48:23 +0100

    FInal Deploy

--- bisi/index.js
+++ bisi/index.js
@@ -8,10 +8,10 @@ app.use(express.json())
 // Get all products
 app.get('/api/products',(req,res)=>{
     const products = [
-        {id:1,name:'Laptop',price:2000},
-        {id:2,name:'Desktop',price:2500},
+        {id:1,name:'Laptop',price:6000},
+        {id:2,name:'Desktop',price:7500},
         {id:3,name:'Phone',price:900},
-        {id:4,name:'Tv',price:3000},
+        {id:4,name:'Netflix',price:3000},
     ]
     res.status(200).json({products})
 })
